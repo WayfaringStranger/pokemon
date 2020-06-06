a4de1c713887f259c4c52a799dd430bc286f3163
WafaringStranger <shubhampaitrik8@gmail.com>
2020-06-06 09:55:22 +0530

    The Pokegame Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Pokedex from "./Pokedex";
-import "./App.css";
+import Pokegame from "./Pokegame";
 
 function App() {
   return (
     <div className="App">
-      <Pokedex />
+      <Pokegame />
     </div>
   );
 }
